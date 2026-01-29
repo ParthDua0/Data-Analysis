be6833303f7c4cad0e91e4187e81d8a3591e4a64
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-01-30 00:09:33 +0530

    more revision

---
+++ Python/Loop.py
@@ -0,0 +1,4 @@
+a = [1,2,3,4,5,6,7]
+
+for item in range(len(a)):
+    print("item at index", item, "is", a[item])
---
+++ Python/String.py
@@ -0,0 +1,8 @@
+text = "hellohellopython is here"
+reversed_text = text[-1:0:-1]
+print(reversed_text)
+
+
+c= text.split(" ")
+print(c)
+print(len(c[0]))
