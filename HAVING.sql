073f91c08bd1338a28d7d5b0bb6db3f0d41219ee
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-06-23 23:46:48 +0530

    Add comments to SQL query for clarity

--- HAVING.sql
+++ HAVING.sql
@@ -8,7 +8,7 @@ HAVING AVG(age)>40
 
 SELECT occupation, AVG(salary)
 FROM parks_and_recreation.employee_salary
-WHERE occupation LIKE '%manager%'
-GROUP BY occupation
-HAVING AVG(salary) > 70000
+WHERE occupation LIKE '%manager%' -- before groupby
+GROUP BY occupation    -- group by
+HAVING AVG(salary) > 70000   -- after group by
 ;
