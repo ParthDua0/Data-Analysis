c7a25e75e4e46d3da7d0f38f19bf50efe56897af
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-07-20 23:21:24 +0530

    Correct SQL syntax for ORDER BY statements

--- GROUP BY- ORDER BY.sql
+++ GROUP BY- ORDER BY.sql
@@ -15,14 +15,14 @@ group by occupation, salary
 
 -- ORDER BY (corrected)
 
-SELECT *
-FROM parks_and_recreation.employee_demographics
-ORDER BY gender, age  
+select *
+from parks_and_recreation.employee_demographics
+order by gender, age  
 ;
 
-SELECT *
-FROM parks_and_recreation.employee_demographics
-ORDER BY 5, 4 
+select *
+from parks_and_recreation.employee_demographics
+order by 5, 4 
 ;
 
 -- GROUP BY and ORDER BY
