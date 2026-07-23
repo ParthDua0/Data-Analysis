d5e883525fcd64386f47b12a4912913aac7737c2
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-07-23 23:08:43 +0530

    Refactor SQL queries for clarity and consistency

--- HAVING.sql
+++ HAVING.sql
@@ -1,12 +1,10 @@
 -- HAVING VS WHERE
 
-select gender, AVG(age)
-from parks_and_recreation.employee_demographics
+select gender, AVG(age) from parks_and_recreation.employee_demographics
 group by gender
 having AVG(age)>40;
 
-select occupation, AVG(salary)
-from parks_and_recreation.employee_salary
+select occupation, AVG(salary)from parks_and_recreation.employee_salary
 where occupation like '%manager%' -- before groupby
 group by occupation    -- group by
 having AVG(salary) > 70000;   -- after group by
