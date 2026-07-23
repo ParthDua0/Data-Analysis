98f354ea4eb3748761af60c839681d4ab90401f0
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-07-23 23:08:16 +0530

    Refactor SQL queries for GROUP BY and ORDER BY
    Removed unnecessary line breaks and corrected formatting in SQL queries.

--- GROUP BY- ORDER BY.sql
+++ GROUP BY- ORDER BY.sql
@@ -1,33 +1,23 @@
 -- GROUP BY (new updated file)
 
-select *
-from parks_and_recreation.employee_demographics;
+select * from parks_and_recreation.employee_demographics;
 
-select gender , AVG(age), MAX(age), MIN(age)
-from parks_and_recreation.employee_demographics
-group by gender
-;
+select gender , AVG(age), MAX(age), MIN(age) from parks_and_recreation.employee_demographics
+group by gender;
 
-select occupation, salary
-from parks_and_recreation.employee_salary
-group by occupation, salary
-;
+select occupation, salary from parks_and_recreation.employee_salary
+group by occupation, salary;
 
 -- ORDER BY (corrected)
 
-select *
-from parks_and_recreation.employee_demographics
-order by gender, age  
-;
+select * from parks_and_recreation.employee_demographics
+order by gender, age;
 
-select *
-from parks_and_recreation.employee_demographics
-order by 5, 4 
-;
+select * from parks_and_recreation.employee_demographics
+order by 5, 4;
 
 -- GROUP BY and ORDER BY
 
 select * from parks_and_recreation.employee_demographics
 group by occupation, salary
-order by 5, 4
-;
+order by 5, 4;
