2b61c6e66eb943795fa7201b1d688097eba64085
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-07-29 09:58:38 +0530

    Normalize SQL queries to lowercase

--- SELECT.sql
+++ SELECT.sql
@@ -7,11 +7,11 @@ from parks_and_recreation.employee_demographics demo;
 select first_name, last_name, birth_date, age, age+10, (age+10)*10
 from parks_and_recreation.employee_demographics demo;
 
-SELECT distinct gender
-FROM parks_and_recreation.employee_demographics;
+select distinct gender
+from parks_and_recreation.employee_demographics;
 
-SELECT DISTINCT first_name,gender
-FROM parks_and_recreation.employee_demographics;
+select distinct first_name,gender
+from parks_and_recreation.employee_demographics;
 
-SELECT count(*) FROM park_and_recreation.employee_demographics
+select count(*) from park_and_recreation.employee_demographics
 group by gender;
