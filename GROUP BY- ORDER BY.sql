cd0c6ec294b92435b542fbcd3ebd761643663cc6
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-08-08 10:39:32 +0530

    Add additional query to select gender and age

--- GROUP BY- ORDER BY.sql
+++ GROUP BY- ORDER BY.sql
@@ -23,3 +23,6 @@ order by 5, 4;
 select * from parks_and_recreation.employee_demographics
 group by occupation, salary
 order by 5, 4;
+
+select gender, age from parks_and_recreation.employee_demographics
+order by 2;
