e31a7d38fe96c5bb70c90ae328c0570835012c74
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-08-26 23:28:50 +0530

    Update HAVING.sql with comments on SQL clauses
    Clarified use of HAVING and WHERE clauses in SQL queries.

--- HAVING.sql
+++ HAVING.sql
@@ -2,7 +2,7 @@
 
 select gender, AVG(age) from parks_and_recreation.employee_demographics
 group by gender
-having AVG(age)>40;
+having AVG(age)>40; --used after grouping 
 
 select occupation, AVG(salary)from parks_and_recreation.employee_salary
 where occupation like '%manager%' -- use before groupby
