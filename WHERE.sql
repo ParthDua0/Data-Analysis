4ec2983ac4cfee5f900cafbe565b31aedfdb3c80
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-08-30 23:23:32 +0530

    Add filter for first name in employee demographics query

--- WHERE.sql
+++ WHERE.sql
@@ -25,4 +25,5 @@ select * from parks_and_recreation.employee_demographics
 where birth_date = '1989';
 
 select * from parks_and_recreation.employee_demographics
-where birth_date between 1980 and 1990;
+where birth_date between 1980 and 1990
+and first_name like 'a%';
