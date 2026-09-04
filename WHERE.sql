d2454de55fe4a192de517dff02279e13da07d946
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-09-04 23:34:01 +0530

    Fix SQL syntax for employee demographics query

--- WHERE.sql
+++ WHERE.sql
@@ -25,5 +25,4 @@ select * from parks_and_recreation.employee_demographics
 where birth_date = '1989';
 
 select * from parks_and_recreation.employee_demographics
-where birth_date between 1980 and 1990
-and first_name like 'a%';
+where birth_date between 1980 and 1990;
