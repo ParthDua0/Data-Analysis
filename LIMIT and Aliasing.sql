0284612c18a157b12a32c1dd6f512f95e520a628
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-09-13 20:47:01 +0530

    Add OFFSET example to LIMIT and Aliasing.sql

--- LIMIT and Aliasing.sql
+++ LIMIT and Aliasing.sql
@@ -16,7 +16,7 @@ select gender, AVG(age) avg_age from parks_and_recreation.employee_demographics
 group by gender
 having avg_age > 40;
 
---learning offset in sql with  multiple examples
+--Offset
 select * from parks_and_recreation.employee_demographics
 order by age desc
 limit 3 OFFSET 2; 
